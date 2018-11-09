a49f6d22193d28d8a23706bd7abba068924dd740
lucaspchartier <lucaspchartier@gmail.com>
2018-11-09 11:12:28 -0500

    Finished minor CSS touches and added JS event listeners for square selection. Adding more JS code next.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,9 +10,40 @@ const events = require('./events.js')
 
 // document on ready
 $(() => {
-  // JS code
+  // JS ajax code
   $('#sign-up-form').on('submit', events.onSignUp)
   $('#sign-in-form').on('submit', events.onSignIn)
   $('#change-password-form').on('submit', events.onChangePassword)
   $('#sign-out-button').on('click', events.onSignOut)
+})
+
+$(() => {
+  // JS game code
+  $('#0').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#1').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#2').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#3').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#4').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#5').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#6').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#7').on('click', function (event) {
+    $(event.target).text()
+  })
+  $('#8').on('click', function (event) {
+    $(event.target).text()
+  })
 })
