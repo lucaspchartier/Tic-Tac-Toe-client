37a97d9fccf97770ef2c0ec82705ca367c2fdb06
lucaspchartier <lucaspchartier@gmail.com>
2019-04-17 19:33:17 -0400

    Changed signInSuccess and signOutSuccess function a bit.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,9 +3,10 @@
 const store = require('./../store.js')
 
 const signUpSuccess = function (signUpResponse) {
-  $('.auth-message').html('Sign up successful!')
+  $('.auth-message').html('Signed up successfully!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
+  $('.sign-up-form').slideUp(500)
   setTimeout(function () {
     $('.auth-message').empty()
   }, 2000)
@@ -22,14 +23,16 @@ const failure = function (failureResponse) {
 
 const signInSuccess = function (signInResponse) {
   store.user = signInResponse.user
-  $('.auth-message').html('Login successful!')
+  $('.auth-message').html('Logged in successfully!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
-  $('#change-password-form').removeClass('hidden')
-  $('#sign-out-button').removeClass('hidden')
+  $('.sign-up-form').slideUp(500)
+  $('.sign-in-form').slideUp(500)
+  $('#change-password-btn').show()
+  $('#sign-out-btn').show()
+  $('#sign-up-btn').hide()
+  $('#sign-in-btn').hide()
   $('.game-container').removeClass('hidden')
-  $('#sign-up-form').addClass('hidden')
-  $('#sign-in-form').addClass('hidden')
   setTimeout(function () {
     $('.auth-message').empty()
   }, 2000)
@@ -39,20 +42,24 @@ const changePasswordSuccess = function (changePasswordResponse) {
   $('.auth-message').html('Password change successful!')
   $('.auth-message').addClass('success-message')
   $('.auth-message').removeClass('error-message')
+  $('#change-password-form').slideUp(500)
   setTimeout(function () {
     $('.auth-message').empty()
   }, 2000)
 }
 
 const signOutSuccess = function (signOutResponse) {
-  $('.auth-message').html('Logout successful!')
-  $('.auth-message').addClass('success-message')
-  $('.auth-message').removeClass('error-message')
-  $('#change-password-form').addClass('hidden')
-  $('#sign-out-button').addClass('hidden')
-  $('.game-container').addClass('hidden')
-  $('#sign-up-form').removeClass('hidden')
-  $('#sign-in-form').removeClass('hidden')
+  store.over = true
+  $('#game-message').html('Logged out successfully!')
+  $('#game-message').removeClass('error-message')
+  $('#game-message').addClass('success-message')
+  $('#change-password-btn').hide()
+  $('#change-password-form').slideUp(500)
+  $('#sign-out-btn').hide()
+  $('#sign-up-btn').show()
+  $('#sign-in-btn').show()
+  $('.game-container').fadeOut(500)
+  $('#change-password-form').trigger('reset')
   setTimeout(function () {
     $('.auth-message').empty()
   }, 2000)
