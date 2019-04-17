545bec913b458456a50cfd09d158af47b3d34b2c
lucaspchartier <lucaspchartier@gmail.com>
2019-04-17 09:55:55 -0400

    Moved curl scripts under auth and game folders.

--- curl-scripts/get-game.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl "https://tic-tac-toe-wdi.herokuapp.com/games" \
-  --include \
-  --request GET \
-  --header "Content-Type: application/json" \
-  --header "Authorization: Token token="${TOKEN}"" \
-
-echo
