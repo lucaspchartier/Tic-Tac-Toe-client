53057acdb90c3467c026d027b9bdba5ece25d3b2
lucaspchartier <lucaspchartier@gmail.com>
2019-04-18 09:46:03 -0400

    Added moved content from deleted drawtest.js file.

--- assets/scripts/game/gamelogic.js
+++ assets/scripts/game/gamelogic.js
@@ -118,6 +118,24 @@ const gameLogic = () => {
   }
 }
 
+// const tie = ['X', 'X', 'O', 'X', 'O', 'O', 'O', 'X', 'X']
+//
+// const allCellsTaken = function () {
+//   // return true if cells are taken
+//   // (x == 5 || y == 5)
+//   return (tie[0] === 'X' || tie[0] === 'O') &&
+//   (tie[1] === 'X' || tie[1] === 'O') &&
+//   (tie[2] === 'X' || tie[2] === 'O') &&
+//   (tie[3] === 'X' || tie[3] === 'O') &&
+//   (tie[4] === 'X' || tie[4] === 'O') &&
+//   (tie[5] === 'X' || tie[5] === 'O') &&
+//   (tie[6] === 'X' || tie[6] === 'O') &&
+//   (tie[7] === 'X' || tie[7] === 'O') &&
+//   (tie[8] === 'X' || tie[8] === 'O')
+// }
+//
+// console.log('This cell is taken', allCellsTaken())
+
 module.exports = {
   gameLogic
 }
